431fa34b15a725df0b7d387130da0e311725d02c
Alberto Basalo <albertobasalo@hotmail.com>
2018-11-26 20:26:03 +0100

    WellcomeModule  <lib-wellcome>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { WellcomeModule } from 'wellcome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, WellcomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/library.md
+++ src/app/library.md
@@ -2,3 +2,8 @@ ng g library wellcome
 
 "build:wellcome": "ng build wellcome",
 
+"wellcome": "file:dist/wellcome/wellcome-0.0.1.tgz",
+
+npm i wellcome
+
+import { WellcomeModule } from 'wellcome';
