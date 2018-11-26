363d08f422c19a3aa58b25f84aba5ff02536c071
Alberto Basalo <albertobasalo@hotmail.com>
2018-11-26 18:01:32 +0100

    ng add @ngrx/effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { metaReducers, rootReducers } from './store/root';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, StoreModule.forRoot(rootReducers, { metaReducers })],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, StoreModule.forRoot(rootReducers, { metaReducers }), EffectsModule.forRoot([AppEffects])],
   providers: [],
   bootstrap: [AppComponent]
 })
