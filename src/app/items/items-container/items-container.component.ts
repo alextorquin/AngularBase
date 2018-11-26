3c3b081a3f2a0d02fd09b19f1d4d3a45019829a5
Alberto Basalo <albertobasalo@hotmail.com>
2018-11-26 18:19:15 +0100

    items-container.ts items-container.html

--- src/app/items/items-container/items-container.component.ts
+++ src/app/items/items-container/items-container.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { GetAll } from '../../store/items/items.actions';
+import { RootState } from '../../store/root/root.state';
 import { ItemsApiService } from '../items-api.service';
 
 @Component({
@@ -10,12 +13,14 @@ import { ItemsApiService } from '../items-api.service';
 })
 export class ItemsContainerComponent implements OnInit {
   public items$: Observable<any[]>;
-  constructor(private itemsApiService: ItemsApiService) {}
+  public itemsRedux$: Observable<any[]>;
+  constructor(private itemsApiService: ItemsApiService, private store: Store<RootState>) {}
 
   public ngOnInit() {
     this.items$ = this.itemsApiService.getAll();
+    this.store.dispatch(new GetAll());
+    this.itemsRedux$ = this.store.select('items', 'items');
   }
-
 
   public onSave(newItem) {
     this.items$ = this.itemsApiService.post(newItem).pipe(switchMap(() => this.itemsApiService.getAll()));
